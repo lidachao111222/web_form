91b6eb39db46dc8abf6f8e57ebfbe153b7009e86
lidachao111222 <chao.li@usask.ca>
2024-07-30 16:14:17 -0700

    [refactor] create function events.

--- h5/index.js
+++ h5/index.js
@@ -22,58 +22,76 @@ let phoneNumber = document.querySelector("#canadianPhoneNumber");
 // get button element
 let submitButton = document.querySelector("#subButton");
 
-// birth input event
-dateOfBirth.addEventListener("blur", function () {
-  // get selected date
-  const selectedDate = new Date(this.value);
-  selectedDate.setHours(0, 0, 0, 0);
-  // get current date
-  const currentDate = new Date();
-  currentDate.setHours(0, 0, 0, 0);
-  // compare
-  if (currentDate.valueOf() < selectedDate.valueOf()) {
-    alert("please select a correct date");
-    this.value = "";
-  }
-});
+const validateDateOfBirth = () => {
+  // birth input event
+  dateOfBirth.addEventListener("blur", function () {
+    // get selected date
+    const selectedDate = new Date(this.value);
+    selectedDate.setHours(0, 0, 0, 0);
+    // get current date
+    const currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0);
+    // compare
+    if (currentDate.valueOf() < selectedDate.valueOf()) {
+      alert("please select a correct date");
+      this.value = "";
+    }
+  });
+};
 
-// email input event
-emailAddress.addEventListener("blur", function () {
-  // check if is a email email address.
-  const emailReg = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,4}$/;
-  const emailValue = emailAddress.value.trim();
-  // First check if the email value is empty
-  if (emailValue.length === 0) {
-    return;
-  }
-  // Then check if it matches the email pattern
-  if (!emailReg.test(emailValue)) {
-    // alert("This is not a correct email address, please type again.");
-    emailAddress.classList.add("is-invalid");
-    // this.value = "";
-  }
-});
+const validateEmail = () => {
+  // email input event
+  emailAddress.addEventListener("blur", function () {
+    // check if is a email email address.
+    const emailReg = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,4}$/;
+    const emailValue = emailAddress.value.trim();
+    // First check if the email value is empty
+    if (emailValue.length === 0) {
+      return;
+    }
+    // Then check if it matches the email pattern
+    if (!emailReg.test(emailValue)) {
+      // alert("This is not a correct email address, please type again.");
+      emailAddress.classList.add("is-invalid");
+      // this.value = "";
+    }
+  });
 
-// remove the warning if the length is zero
-emailAddress.addEventListener("keyup", function () {
-  const emailValue = emailAddress.value.trim();
-  if (emailValue.length === 0) {
-    emailAddress.classList.remove("is-invalid");
-  }
-});
+  // remove the warning if the length is zero
+  emailAddress.addEventListener("keyup", function () {
+    const emailValue = emailAddress.value.trim();
+    if (emailValue.length === 0) {
+      emailAddress.classList.remove("is-invalid");
+    }
+  });
+};
 
-// submit button event
-submitButton.addEventListener("click", function () {
-  // if name is empty ask for input
-  if (applicantName.value.length === 0) {
-    alert("the Applicant Name is required, please enter your name");
-    return;
-  }
+const handleSubmit = () => {
+  // submit button event
+  submitButton.addEventListener("click", function () {
+    // if name is empty ask for input
+    if (applicantName.value.length === 0) {
+      alert("the Applicant Name is required, please enter your name");
+      return;
+    }
 
-  // console.log(applicantName.value);
-  // console.log(maritalStatus.value);
-  // console.log(canadianAddress.value);
-  // console.log(dateOfBirth.value);
-  // console.log(emailAddress.value);
-  // console.log(phoneNumber.value);
-});
+    // console.log(applicantName.value);
+    // console.log(maritalStatus.value);
+    // console.log(canadianAddress.value);
+    // console.log(dateOfBirth.value);
+    // console.log(emailAddress.value);
+    // console.log(phoneNumber.value);
+  });
+};
+
+function main() {
+  console.log(cityList);
+
+  validateDateOfBirth();
+
+  validateEmail();
+
+  handleSubmit();
+}
+
+main();
